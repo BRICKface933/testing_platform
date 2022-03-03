fe82383a8ae453e8a7fdfe081a504810564da1d2
BRICKface933 <tychinin.danila@yandex.ru>
2022-03-03 13:15:46 +0300

    BRICKface933

--- api/api.js
+++ api/api.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
 
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    database: "test",
+    user: "root",
+    password: "Pa$$w0rd"
+});
+
 router.get("/", function(request, response){
+    connection.connect(function(err){
+        if(err){
+            console.log("DB Danger!")
+        }else{
+            console.log("DB Succesful")
+        }
+    })
     response.send("<h1>API page</h1>")
 })
 
